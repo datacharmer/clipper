487e9aac5d52c563d3465b2e086ada114798f6ab
Giuseppe Maxia <g.maxia@gmail.com>
2024-07-12 15:26:56 +0200

    Change main func to call a int-returning function
    Testcript requires a function returning an int in order
    to simulate the executable within the test.

--- cli/main.go
+++ cli/main.go
@@ -9,12 +9,12 @@ import (
 	"github.com/supitsdu/clipper/cli/reader"
 )
 
-func main() {
+func RunMain() int {
 	config := options.ParseFlags() // Parse command-line flags
 
 	if config.ShowVersion {
 		fmt.Printf("Clipper %s\n", options.GetVersion())
-		os.Exit(0)
+		return 0
 	}
 
 	writer := clipper.DefaultClipboardWriter{} // Create the default clipboard writer
@@ -24,8 +24,13 @@ func main() {
 	msg, err := clipper.Run(reader, writer) // Run the main Clipper logic
 	if err != nil {
 		fmt.Printf("Error %s\n", err)
-		os.Exit(1)
+		return 1
 	}
 
 	fmt.Println(msg)
+	return 0
+}
+
+func main() {
+	os.Exit(RunMain())
 }
